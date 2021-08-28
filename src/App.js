a01d566e11be52ccb0006a43ae4bbaf5814381d2
ArtexCoder <81457945+ArtexCoder@users.noreply.github.com>
2021-08-29 07:59:40 +1000

    Added search form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
+import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <small>
+          <footer className="App-footer">Coded by Meiling Aw</footer>
+        </small>
+      </div>
     </div>
   );
 }
-
