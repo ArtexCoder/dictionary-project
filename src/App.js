aafb005fe431646b28ada7963346b9529a3e8bf5
ArtexCoder <81457945+ArtexCoder@users.noreply.github.com>
2021-09-04 09:53:28 +1000

    Styled search form and included a default word

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
       <div className="container">
         <header className="App-header"></header>
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="imagination" />
         </main>
         <small>
           <footer className="App-footer">
